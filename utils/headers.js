9b659bb860af94bce35505953087b9e3bcbfe3da
LustyCodes <131748822+LustyCodes@users.noreply.github.com>
2025-02-16 01:02:34 +0600

    Create headers.js

---
+++ utils/headers.js
@@ -0,0 +1,10 @@
+// Common headers for making requests
+const headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Cache-Control': 'max-age=0'
+};
+
+export { headers };
